37a9a35ba6468987de3d35d42f94334542b97584
Gary Kellstrom, III <garyekellstromiii@gmail.com>
2026-06-28 11:25:21 -0400

    fixing npm start command again 5

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   // outputFileTracingRoot silences the monorepo lockfile warning in next lint / next build
   outputFileTracingRoot: require("path").join(__dirname, "../../"),
 };
